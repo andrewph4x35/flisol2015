1ed6de77692fd432e563946631808690744b5730
ubuntu <g2418469@trbvm.com>
2015-04-25 10:30:36 -0300

    teste

---
+++ FLISOL.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="banner"></div>
+	<div clss="nav">
+	<ul><li><
+	</div>
+	<div class="conteudo"></div>
+	<div class="rodape"></div>
+</body>
+</html>
